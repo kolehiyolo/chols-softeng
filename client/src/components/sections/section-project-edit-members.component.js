8adc36b378bb42d71da7dfe88cbc0b8309a37d15
Tristan Sean Paul Perfecto Cinco <tristanseanpaul.cinco@gmail.com>
2023-12-14 06:15:40 +0800

    Bootstrap Modal Working
    1. Holy crap Bootstrap modal works lol
    2. Very easy to setup, thanks ChatGPT
    3. Now to make it actually useful

--- client/src/components/sections/section-project-edit-members.component.js
+++ client/src/components/sections/section-project-edit-members.component.js
@@ -1,9 +1,11 @@
 // * Dependencies
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 // * Importing other Components
 import ItemEditMember from '../items/item-edit-member.component.js';
+import TestModal from '../popup/popup-v1.component.js';
 // import ItemTask from '../items/item-task.component.js';
 // import CardTask from './card-task.component.js';
 
@@ -12,6 +14,7 @@ import './section-project-edit-members.component.scss';
 
 export default function SectionProjectEditMembers(props) {
   const [currentUserFriendsData, setCurrentUserFriendsData] = useState();
+  const [showModal, setShowModal] = useState(false);
 
   // TODO If user adds a new member, their friend list should pop up
   
@@ -22,12 +25,34 @@ export default function SectionProjectEditMembers(props) {
     []
   );
 
+  function handleShow() {
+    setShowModal(true);
+  };
+
+  function handleClose() {
+    setShowModal(false);
+  };
+
+  function onAddMemberClick() {
+    console.log('onAddMemberClick');
+    handleShow();
+  };
+
   // * Render
   return (
     <div className='section-project-edit-members'>
-      <button className='add-member'>
-        Add Member
-      </button>
+      <div className='new-member'>
+        <Button
+          variant="primary"
+          onClick={onAddMemberClick}
+        >
+          Add Member
+        </Button>
+        <TestModal
+          showModal={showModal}
+          handleClose={handleClose}
+        />
+      </div>
       <div className='members'>
         {             
           (props.newMembersData)
