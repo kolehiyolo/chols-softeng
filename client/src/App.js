0841f8e8e70d0fc5688d78183ddbd830912b0faf
Tristan Sean Paul Perfecto Cinco <tristanseanpaul.cinco@gmail.com>
2023-12-13 09:15:46 +0800

    Project react-routes ready
    1. The react-routes for the projects stuff are ready, I guess
    2. I'll start building page-projects.component.js first

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component.js";
-import ListProjects from "./components/list-projects.component.js";
+import Projects from "./components/page-projects.component.js";
+import Project from "./components/page-project.component.js";
+import ProjectEdit from "./components/page-project-edit.component.js";
+import ProjectNew from "./components/page-project-new.component.js";
 // import ListTasks from "./components/list-tasks.component.js";
 // import ListUsers from "./components/list-users.component.js";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState('65773b179407d34564bd6ed9');
+
   return (
     <Router>
       <Navbar />
       <br />
       <div className="container">
       <Routes>
-        <Route path="/" exact element={<ListProjects />} />
-        {/* <Route path="/tasks" exact element={<ListTasks />} /> */}
-        {/* <Route path="/users" exact element={<ListUsers />} /> */}
+        <Route path="/" exact element={<Projects currentUser={currentUser}/>} />
+        <Route path="/projects" exact element={<Projects currentUser={currentUser}/>} />
+        <Route path="/project/:id" render={(props) => <Project currentUser={currentUser} projectId={props.match.params.id} />} />
+        <Route path="/project/edit/:id" render={(props) => <ProjectEdit currentUser={currentUser} projectId={props.match.params.id} />} />
+        <Route path="/projects/new" exact element={<Projects currentUser={currentUser}/>} />
       </Routes>
       </div>
     </Router>
